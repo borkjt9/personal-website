d4edbaaaff3747fa7a279536dd45aa491795136f
borkjt9 <borkjt9@gmail.com>
2018-03-22 16:50:31 -0400

    Cleaned up footer

--- src/footer/footer.js
+++ src/footer/footer.js
@@ -1,31 +1,30 @@
 import React from 'react';
+import map from 'lodash/map';
 import './footer.scss';
 
-const Footer = () => (
-  <div className="footer">
-    <div className="footer-links">
-      <span className="footer-link">
-        <a className="mail footer-link__image" href="mailto:borkjt9@gmail.com">Mail</a>
-        <h5 className="footer-link__name">Email</h5>
-      </span>
-      <span className="footer-link">
-        <a className="linkedin footer-link__image" href="https://www.linkedin.com/in/john-borkowski-cfa-8533bb25/">LinkedIn</a>
-        <h5 className="footer-link__name">LinkedIn</h5>
-      </span>
-      <span className="footer-link">
-        <a className="resume footer-link__image" href="https://johnborkowski.me/pdfs/resume-john-borkowski.pdf" target="_blank" rel="noopener noreferrer">Resume</a>
-        <h5 className="footer-link__name">Resume</h5>
-      </span>
-      <span className="footer-link">
-        <a className="github footer-link__image" href="https://github.com/borkjt9">Github</a>
-        <h5 className="footer-link__name">Github</h5>
-      </span>
-      <span className="footer-link">
-        <a className="angel-list footer-link__image" href="https://angel.co/john-borkowski-1">Angel List</a>
-        <h5 className="footer-link__name">Angelist</h5>
-      </span>
+
+function Footer() {
+
+  const items = [
+    {'name': 'Email', 'href': "mailto:borkjt9@gmail.com"},
+    {'name': 'LinkedIn', 'href': "https://www.linkedin.com/in/john-borkowski-cfa-8533bb25/"},
+    {'name': 'Resume', 'href': "https://johnborkowski.me/pdfs/resume-john-borkowski.pdf"},
+    {'name': 'Github', 'href': "https://github.com/borkjt9"},
+    {'name': 'Angelist', 'href': "https://angel.co/john-borkowski-1"}
+  ]
+  const renderedItems = map(items, item => (
+    <span className="footer-link">
+      <a className={`${item.name.toLowerCase()} footer-link__image`} href={item.href} target="_blank" rel="noopener noreferrer" />
+      <h5 className="footer-link__name">{item.name}</h5>
+    </span>
+  ));
+  return (
+    <div className="footer">
+      <div className="footer-links">
+        {renderedItems}
+      </div>
     </div>
-  </div>
-);
+  )
+};
 
 export default Footer;
