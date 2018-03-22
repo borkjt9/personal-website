f4004cee03d15ca7619f02448f621707583a5e77
borkjt9 <borkjt9@gmail.com>
2018-03-22 17:48:25 -0400

    Made footer links open in new page. Added footer tests

---
+++ src/__tests__/footer.test.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import Enzyme, {shallow, mount} from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16';
+import Footer from '../footer/footer';
+
+Enzyme.configure({'adapter': new Adapter()});
+
+const items = [
+  { name: 'Email', href: 'mailto:borkjt9@gmail.com' },
+  { name: 'LinkedIn', href: 'https://www.linkedin.com/in/john-borkowski-cfa-8533bb25/' },
+  { name: 'Resume', href: 'https://johnborkowski.me/pdfs/resume-john-borkowski.pdf' },
+  { name: 'Github', href: 'https://github.com/borkjt9' },
+  { name: 'Angelist', href: 'https://angel.co/john-borkowski-1' },
+];
+
+describe('footer', () => {
+  let component;
+  beforeEach(() => {
+    component = shallow(<Footer />)
+  });
+
+  it('renders all items', () => {
+    items.forEach((item) => {
+      component.contains(
+        <span className="footer-link">
+          <a
+            className={`${item.name.toLowerCase()} footer-link__image`}
+            href={item.href}
+            target="_blank"
+            rel="noopener noreferrer">{item.name}
+          </a>
+          <h5 className="footer-link__name">{item.name}</h5>
+      </span>)
+    });
+
+    it('item images change on hover', () => {
+
+    });
+  });
+})
--- src/footer/footer.js
+++ src/footer/footer.js
@@ -3,18 +3,19 @@ import map from 'lodash/map';
 import './footer.scss';
 
 
-function Footer() {
 
+function Footer() {
   const items = [
-    {'name': 'Email', 'href': "mailto:borkjt9@gmail.com"},
-    {'name': 'LinkedIn', 'href': "https://www.linkedin.com/in/john-borkowski-cfa-8533bb25/"},
-    {'name': 'Resume', 'href': "https://johnborkowski.me/pdfs/resume-john-borkowski.pdf"},
-    {'name': 'Github', 'href': "https://github.com/borkjt9"},
-    {'name': 'Angelist', 'href': "https://angel.co/john-borkowski-1"}
-  ]
+    { name: 'Email', href: 'mailto:borkjt9@gmail.com' },
+    { name: 'LinkedIn', href: 'https://www.linkedin.com/in/john-borkowski-cfa-8533bb25/' },
+    { name: 'Resume', href: 'https://johnborkowski.me/pdfs/resume-john-borkowski.pdf' },
+    { name: 'Github', href: 'https://github.com/borkjt9' },
+    { name: 'Angelist', href: 'https://angel.co/john-borkowski-1' },
+  ];
+  
   const renderedItems = map(items, item => (
-    <span className="footer-link">
-      <a className={`${item.name.toLowerCase()} footer-link__image`} href={item.href} target="_blank" rel="noopener noreferrer" />
+    <span key={item.name} className="footer-link">
+      <a className={`${item.name.toLowerCase()} footer-link__image`} href={item.href} target="_blank" rel="noopener noreferrer">{item.name}</a>
       <h5 className="footer-link__name">{item.name}</h5>
     </span>
   ));
@@ -24,7 +25,7 @@ function Footer() {
         {renderedItems}
       </div>
     </div>
-  )
-};
+  );
+}
 
-export default Footer;
+export default Footer
