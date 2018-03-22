5df1631c6f7701de463b456eb76d87f3517338ed
borkjt9 <borkjt9@gmail.com>
2018-03-22 16:00:32 -0400

    Added tests / cleaned up tests for landing page

--- src/__tests__/landing-page.test.js
+++ src/__tests__/landing-page.test.js
@@ -1,13 +1,7 @@
 import React from 'react';
-// import Enzyme, { shallow, mount } from 'enzyme';
-import TestUtils from 'react-dom/test-utils'; // ES6
-//import {renderComponent} from '../test_helper';
-// import { shallowToJson } from 'enzyme-to-json';
-// import Adapter from 'enzyme-adapter-react-16';
 import { BrowserRouter} from 'react-router-dom';
-import renderer from 'react-test-renderer';
 import LandingPage from '../landing-page/landing-page';
-import Enzyme, {shallow} from 'enzyme';
+import Enzyme, {shallow, mount, simulate} from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 Enzyme.configure({ adapter: new Adapter() });
@@ -15,82 +9,47 @@ Enzyme.configure({ adapter: new Adapter() });
 describe('Landing Page', () => {
 
   let component;
-  let tree;
-  let renderedPage;
 
   beforeEach(() => {
-    component = renderer.create(
+    component = mount(
       <BrowserRouter>
-        <LandingPage />
+        <LandingPage/>
       </BrowserRouter>
-
     )
-    tree = component.toJSON()
-
-    renderedPage = TestUtils.renderIntoDocument(
-          <BrowserRouter>
-            <LandingPage />
-          </BrowserRouter>
-        );
-
   });
 
   it('renders john borkowski', () => {
-    const name_class = TestUtils.findRenderedDOMComponentWithClass(renderedPage, 'header__home-link__text')
-    // const wrapper = shallow(name_class)
-    expect(name_class.textContent).toEqual('john borkowski');
-    // expect(component).toContain('john borkowski')
+    const title = component.find('.header__home-link__text')
+    expect(title.text()).toEqual('john borkowski');
   });
 
+  it('renders correct header at start', () => {
+    const header = component.find('.header .not-top-bar')
+    expect(header.exists()).toBe(true);
+  });
+
+  it('renders correct header on scroll', ()=> {
+    component.find('LandingPage').instance().setState({scrollPositionY: 200})
+    component.update()
+    expect(component.find('.header .is-top-bar').exists()).toBe(true);
+    component.find('LandingPage').instance().setState({scrollPositionY: 0})
+    component.update()
+    expect(component.find('.header .is-top-bar').exists()).toBe(false);
+  })
+
   it('switches to correct section on click', () => {
-    const header = TestUtils.findRenderedDOMComponentWithClass(renderedPage, 'header')
-    const buttons = TestUtils.scryRenderedDOMComponentsWithTag(renderedPage, 'button')
+    const buttons = component.find('button')
     buttons.forEach((button) => {
-      TestUtils.Simulate.click(button);
-      const portfolioSection = TestUtils.scryRenderedDOMComponentsWithClass(renderedPage, 'portfolio')
-      const aboutSection = TestUtils.scryRenderedDOMComponentsWithClass(renderedPage, 'about')
-        if (button.textContent === 'Portfolio') {
+      button.simulate('click')
+      const portfolioSection = component.find('.portfolio')
+      const aboutSection = component.find('.about')
+      if (button.text() === 'Portfolio') {
         expect(portfolioSection.length).toEqual(1)
         expect(aboutSection.length).toEqual(0)
-        } else {
+      } else {
           expect(portfolioSection.length).toEqual(0)
           expect(aboutSection.length).toEqual(1)
         }
     });
   });
-
-  it('renders header', () => {
-
-    const name_class = TestUtils.scryRenderedDOMComponentsWithClass(renderedPage, 'header')
-    // const wrapper = shallow(name_class)
-    expect(name_class.length).toEqual(1);
-    // expect(component).toContain('john borkowski')
-  });
-
-  // it('renders correctly', () => {
-  //
-  //   expect(tree).toMatchSnapshot()
-  // });
 });
-
-// import React from 'react';
-// import Enzyme, { shallow } from 'enzyme';
-//
-// import { shallowToJson } from 'enzyme-to-json';
-// import Adapter from 'enzyme-adapter-react-16';
-//
-// import App from '../jest_examples/app';
-//
-// Enzyme.configure({ adapter: new Adapter() });
-//
-// describe('App Jest Example', () => {
-//   it('contains React text', () => {
-//     const component = shallow(<App />);
-//     expect(component.text()).toContain('React Simple Starter');
-//     // expect(component).toContain('john borkowski')
-//   });
-//   it('should render correctly', () => {
-//     const output = shallow(<App />);
-//     expect(shallowToJson(output)).toMatchSnapshot();
-//   });
-// });
