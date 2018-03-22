b80b93741a765a4bc3091b93857eb3efe8bb545f
borkjt9 <borkjt9@gmail.com>
2018-03-22 17:49:01 -0400

    Cleaned up tests

--- src/__tests__/portfolio-grid.test.js
+++ src/__tests__/portfolio-grid.test.js
@@ -1,75 +1,34 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import TestUtils from 'react-dom/test-utils';
-import PortfolioGrid from '../portfolio/portfolio-grid/portfolio-grid';
-import renderer from 'react-test-renderer';
-import portfolioArr from '../public-objects/portfolio-arr';
-import Enzyme, {shallow} from 'enzyme';
+import Enzyme, {mount, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
-Enzyme.configure({'adapter': new Adapter() });
+import PortfolioGrid from '../portfolio/portfolio-grid/portfolio-grid';
+import portfolioArr from '../public-objects/portfolio-arr';
+
+Enzyme.configure({ adapter: new Adapter() });
 
 function changePortfolioItem() {
-  console.log('filler')
+  console.log('filler');
 }
 
 let wrapper;
-let renderedPage;
-beforeEach(()=> {
-  wrapper = shallow(<PortfolioGrid changePortfolioItem={changePortfolioItem} />)
+beforeEach(() => {
+  wrapper = shallow(<PortfolioGrid changePortfolioItem={changePortfolioItem} />);
+});
 
-  renderedPage = TestUtils.renderIntoDocument(
-        <PortfolioGrid changePortfolioItem={changePortfolioItem} />
-      );
-
-
-})
 describe('PortfolioGrid', () => {
-  it("renders without crashing", () => {
-    const div = document.createElement("div");
-    ReactDOM.render(<PortfolioGrid
-      changePortfolioItem={changePortfolioItem}
-    />, div);
+  it('includes at least 1 portfolio item', () => {
+    expect(wrapper.find('PortfolioItem').exists()).toBe(true);
   });
 
-  it("includes at least 1 portfolio item", () => {
-      expect(wrapper.find("PortfolioItem").exists()).toBe(true);
+  it('includes the correct number of portfolio items', () => {
+    expect(wrapper.find('PortfolioItem')).toHaveLength(portfolioArr.length);
   });
 
-  it("includes the correct number of portfolio items", () => {
-    expect(wrapper.find("PortfolioItem")).toHaveLength(portfolioArr.length);
-
-  });
-
-  it("includes all the portfolio items", () => {
-    let portfolioItemNames = [];
-    portfolioArr.forEach(item => {
-      portfolioItemNames.push(item.name)
-
-    })
-
-    const renderedItems = TestUtils.scryRenderedDOMComponentsWithClass(renderedPage, 'portfolio__item__desc__title')
-    renderedItems.forEach((item, i) => {
-      expect(item.textContent).toEqual(portfolioItemNames[i])
+  it('includes all the portfolio items', () => {
+    const renderedItemNames = wrapper.find('PortfolioItem')// = TestUtils.scryRenderedDOMComponentsWithClass(renderedPage, 'portfolio__item__desc__title');
+    renderedItemNames.forEach((item, i) => {
+      expect(item.key()).toEqual(portfolioArr[i].name);
     });
-
-    // const renderedNames = wrapper.find("portfolio__item__desc__title").forEach(item => {
-    //   expect(item.textContent).toEqual("Boon Investments")
-    //   //console.log('item', item.find("Boon Investments").toEqual("Boon Investments"))
-    // })
-    // console.log('rendered names: ', renderedNames);
-
-
   });
-
-  it('renders correctly', () => {
-    const component = renderer.create(
-      <PortfolioGrid
-        changePortfolioItem={changePortfolioItem}
-      />
-    );
-
-    const tree = component.toJSON();
-    expect(tree).toMatchSnapshot();
-  })
-})
+});
